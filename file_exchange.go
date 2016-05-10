9aeda83815e8346b85d2be4c46796aed5489f5c1
yoshitsugu_fujii <ishikurasakura@gmail.com>
2016-05-10 17:25:56 +0900

    help追加

--- file_exchange.go
+++ file_exchange.go
@@ -4,6 +4,7 @@ import (
 	"./client"
 	"./server"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"time"
 )
@@ -19,6 +20,15 @@ func main() {
 	if len(os.Args) == 1 || os.Args[1] == "--server" {
 		go server.Run(server_alive)
 		go server.RunUdpServ()
+	} else if os.Args[1] == "help" || os.Args[1] == "-help" || os.Args[1] == "--help" || os.Args[1] == "-h" {
+		fmt.Println("### Help ###")
+		data, err := ioutil.ReadFile("README.md")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(data))
+
+		return
 	} else {
 		err := client.Main(os.Args)
 		if err != nil {
